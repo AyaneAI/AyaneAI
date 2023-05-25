f66b48baa27379826d32be084f0c7d2e97555eb6
wuwenbo1996 <616562636@qq.com>
2023-05-25 21:49:52 +0800

    [FIX] 修改图片存储路径

--- crawler/bing.py
+++ crawler/bing.py
@@ -2,6 +2,7 @@ import requests
 import os
 import datetime
 from github import Github
+import re
 
 # 获取当前日期
 today = datetime.date.today().strftime('%Y%m%d')
@@ -17,13 +18,14 @@ image_url = 'https://www.bing.com' + image_info['url']
 image_title = image_info['title']
 image_copyright = image_info['copyright']
 
-# 存储路径到markdown文件
 # 获取 GitHub API 认证信息
 g = Github(os.environ['GITHUB_TOKEN'])
 # 获取代码仓库
 repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
-# 读取 data.md 文件内容
-file_path = 'data.md'
+
+# 修改 bing_wallpaper.md
+# 读取 bing_wallpaper.md 文件内容
+file_path = 'bing_wallpaper.md'
 
 # 获取文件原内容
 content = ''
@@ -39,3 +41,24 @@ if os.path.exists(file_path):
     repo.update_file(file_path, 'Update data', new_content, repo.get_contents(file_path).sha)
 else:
     repo.create_file(file_path, 'Create data', new_content)
+
+# 修改 README.md
+# 读取 bing_wallpaper.md 文件内容
+file_path = 'README.md'
+
+# 获取文件原内容
+content = ''
+if os.path.exists(file_path):
+    content = repo.get_contents(file_path).decoded_content.decode('utf-8')
+
+# 使用正则表达式匹配注释并提取它们之间的内容
+pattern = r"<!-- BING-WALLPAPER:START -->(.*?)<!-- BING-WALLPAPER:END -->"
+matches = re.findall(pattern, content, re.DOTALL)
+
+# 替换图片
+image_md = "![" + image_info + "](" + image_url + "'" + image_title + "')"
+for match in matches:
+    content = content.replace(match, image_md)
+
+if os.path.exists(file_path):
+    repo.update_file(file_path, 'Update data', content, repo.get_contents(file_path).sha)
